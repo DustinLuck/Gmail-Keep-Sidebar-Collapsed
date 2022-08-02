36412592a937ea056c6ffbfeae5381088ed8ff9e
Dustin Luck <DustinLuck@users.noreply.github.com>
2022-08-02 09:23:06 -0700

    Create Gmail-Keep-Sidebar-Collapsed.user.js

---
+++ Gmail-Keep-Sidebar-Collapsed.user.js
@@ -0,0 +1,27 @@
+/** These settings only take effect when the sidebr is collapsed **/
+/* resize the left aidebar (prevent it from expanding) */
+.aeN.bhZ {
+    min-width: 72px !important;
+    max-width: 72px !important;
+}
+
+/* remove the text from label indicators */
+.aeN.bhZ .TN > .aio {
+    display: none;
+}
+
+/* resize the compose button (prevent it from expanding) */
+.aeN.bhZ.WR .z0 > .L3 {
+	height: 40px;
+	width: 40px;
+	min-width: 40px;
+	border-radius: 16px;
+	font-size: 0;
+	padding: 0;
+    margin-left: 8px;
+    padding-right: 0px;
+}
+
+.aeN.bhZ.WR .z0 > .L3:before {
+    margin: 0px;
+}
